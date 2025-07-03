103ea2f518c042f76c2263a32359d2094350cc0d
LuisFerRodVar <luisfrv1@estudiantec.cr>
2025-07-03 12:57:51 -0600

    chore: clear staged changes
    No staged changes are present. This commit documents an empty staging
    area, possibly as a reset or checkpoint.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -26,6 +26,14 @@ return {
 					},
 				},
 			})
+			vim.keymap.set("n", "<leader>ae", function()
+				vim.cmd("Copilot enable")
+				vim.notify("Copilot enabled")
+			end, { desc = "Enable Copilot" })
+			vim.keymap.set("n", "<leader>ad", function()
+				vim.cmd("Copilot disable")
+				vim.notify("Copilot disabled")
+			end, { desc = "Disable Copilot" })
 		end,
 	},
 	{
